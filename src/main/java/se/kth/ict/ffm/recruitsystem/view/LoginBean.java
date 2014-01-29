0617b9c9ccc519e957f2d1102b4654765cbfa7fa
feducci <feducci@gmail.com>
2014-01-29 18:27:49 +0100

    Merge origin/Federico into Federico
    Conflicts:
    	src/main/webapp/index.xhtml
    	src/main/webapp/template/template.xhtml

---
+++ src/main/java/se/kth/ict/ffm/recruitsystem/view/LoginBean.java
@@ -0,0 +1,79 @@
+/*    
+ *     RecruitSystem - a distributed application to handle job applications.
+ *     Copyright (C) 2014  Federico Klappenbach, Fredrik Johansson, Mikael Tenhunen
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.kth.ict.ffm.recruitsystem.view;
+
+import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Stateless
+@Named
+public class LoginBean {
+
+    @NotNull
+    @Size(min = 1, max = 45)
+    private String username;
+    @NotNull
+    @Size(min = 1, max = 45)
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            request.login(this.username, this.password);
+        } catch (ServletException e) {
+
+            context.addMessage(null, new FacesMessage("Login failed."));
+            return "fail";
+        }
+        return "success";
+    }
+
+    public void logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            request.logout();
+        } catch (ServletException e) {
+
+            context.addMessage(null, new FacesMessage("Logout failed."));
+        }
+    }
+}
