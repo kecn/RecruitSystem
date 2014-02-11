904b21a047eed3de60ae338ce874a00839e93a9e
Mikael Tenhunen <mikael.tenhunen@gmail.com>
2014-02-11 10:59:26 +0100

    register application functionality in place

--- src/main/java/se/kth/ict/ffm/recruitsystem/controller/LanguageBean.java
+++ src/main/java/se/kth/ict/ffm/recruitsystem/controller/LanguageBean.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*    
  *     RecruitSystem - a distributed application to handle job applications.
  *     Copyright (C) 2014  Federico Klappenbach, Fredrik Johansson, Mikael Tenhunen
@@ -61,67 +60,3 @@ public class LanguageBean {
         this.currentLanguage = newLang;
     }
 }
-=======
-/*    
- *     RecruitSystem - a distributed application to handle job applications.
- *     Copyright (C) 2014  Federico Klappenbach, Fredrik Johansson, Mikael Tenhunen
- * 
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- * 
- *     You should have received a copy of the GNU General Public License
- *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package se.kth.ict.ffm.recruitsystem.controller;
-
-import java.util.LinkedHashMap;
-import java.util.Locale;
-import java.util.Map;
-import javax.ejb.Stateless;
-import javax.faces.context.FacesContext;
-
-@Stateless
-public class LanguageBean {
-    private final Map<String,Object> countries;
-    private String currentLanguage;
-    
-    public LanguageBean() {
-            Locale seLocale = new Locale("se");
-            Locale enLocale = new Locale("en");
-            countries = new LinkedHashMap<>();
-            countries.put("English", enLocale); //label, value
-            countries.put("Swedish", seLocale);
-            this.currentLanguage = "se";
-    }
-    public void countryLocaleCodeChanged(String e) {
-        //loop a map to compare the locale code
-        for (Map.Entry<String, Object> entry : countries.entrySet()) {
-            if (entry.getKey().equals(e)) {
-                FacesContext.getCurrentInstance()
-                     .getViewRoot().setLocale((Locale) entry.getValue());
-                
-                if(entry.getKey().equals("English"))
-                    setCurrentLanguage("en");
-                if(entry.getKey().equals("Swedish"))
-                    setCurrentLanguage("se");
-            }
-        }
-    }
-    
-    public String getCurrentLanguage() {
-        return this.currentLanguage;
-    }
-    
-    public void setCurrentLanguage(String newLang){
-        this.currentLanguage = newLang;
-    }
-}
->>>>>>> origin/databasetest
